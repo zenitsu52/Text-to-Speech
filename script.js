d2f9823a5ae34cf92f8c07379844aa5f795eacb0
sahil <sahilgawade46@gmail.com>
2024-06-27 21:12:00 +0530

    new

--- script.js
+++ script.js
@@ -34,15 +34,23 @@ synth.onvoiceschanged = () => {
   const newVoice = document.querySelectorAll('.swiper-slide'); // Replace '.voice-button' with your actual selector
   // Add click event listeners to each voice button
   newVoice.forEach((voiceButton, index) => {
+      let flag=0;
       voiceButton.onclick = () => {
-          console.log('Clicked voice index:', index);
-      
+         
+        if(flag==0){
           // Create SpeechSynthesisUtterance instance
-          const utterance = new SpeechSynthesisUtterance('Hello, world!');
+          const utterance = new SpeechSynthesisUtterance("In a world brimming with possibilities, you shine brightly with your unique blend of talents and passions. Your journey is a tapestry of courage, kindness, and unwavering determination. Embrace each moment with grace, knowing your presence makes a profound difference.");
           utterance.voice = voices[index];
 
           // Speak the utterance
           synth.speak(utterance);
+          flag=1;
+        }
+        else{
+          synth.cancel()
+          flag=0;
+        }
+          
       };
   });
 };
